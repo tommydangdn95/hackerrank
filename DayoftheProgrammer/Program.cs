5e91cbbb135412db6f78b4343afd3ba86ebd952b
Thuy Dang Tran <thuy.dang@getz.co>
2020-03-26 14:37:46 +0700

    DayOfProgrammer Problem

---
+++ DayoftheProgrammer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DayoftheProgrammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2017;
+            Console.WriteLine(CalculateDayProgrammer(year).ToString("dd.MM.yyyy"));
+        }
+
+        private static DateTime CalculateDayProgrammer(int year)
+        {
+            DateTime programmerDay;
+            if(year == 1918)
+            {
+                programmerDay = new DateTime(1918, 09, 26);
+            }
+            else if (year < 1918)
+            {
+                if(year % 4 == 0)
+                {
+                    programmerDay = new DateTime(year, 9, 13);
+                }
+                else
+                {
+                    programmerDay = new DateTime(year, 9, 12);
+                }
+            }
+            else
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    programmerDay = new DateTime(year, 9, 12);
+                }
+                else
+                {
+                    programmerDay = new DateTime(year, 9, 13);
+                }
+            }
+
+            return programmerDay;
+        }
+    }
+}
