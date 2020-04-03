68f57e166dc0baad20c47949847a247942ab09b6
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-03 20:44:17 +0700

    Sock Merchant
    John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.

---
+++ SockMerchant/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SockMerchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            Console.WriteLine(sockMerchant(9, a));
+        }
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            HashSet<int> pairCollec = new HashSet<int>();
+            int pair = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (!pairCollec.Contains(ar[i]))
+                {
+                    pairCollec.Add(ar[i]);
+                }
+                else
+                {
+                    pair++;
+                    pairCollec.Remove(ar[i]);
+                }
+            }
+
+            return pair++;
+        }
+    }
+}
