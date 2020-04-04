f88b971622ce92ed41bcf48507949c0b4bc45a0e
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 11:09:53 +0700

    Drawing Book
    Brie’s Drawing teacher asks her class to open their books to a page number. Brie can either start turning pages from the front of the book or from the back of the book. She always turns pages one at a time. When she opens the book, page  is always on the right side:

---
+++ DrawingBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DrawingBook
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(pageCount(20, 17));
+        }
+
+
+        public static int pageCount(int n, int p)
+        {
+            return Math.Min(p / 2, n / 2 - p / 2);
+        }
+
+
+        static int pageCountV1(int n, int p)
+        {
+            int mid;
+            if (p == n || p == 1)
+                return 0;
+
+            mid = n / 2;
+
+            if (p > mid)
+            {
+                if (n % 2 == 0)
+                {
+                    if ((n - p) % 2 == 0)
+                        return (n - p) / 2;
+                    else
+                        return (n - p) / 2 + 1;
+                }
+                else
+                {
+                    return (n - p) / 2;
+                }
+            }
+            return p / 2;
+        }
+    }
+}
