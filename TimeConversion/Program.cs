592a96501534f8e60104123493bd967f75dd70dc
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-08 10:17:49 +0700

    Time Conversion
    Given a time in -hour AM/PM format, convert it to military (24-hour) time.
    
    Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = timeConversion("07:05:45PM");
+            Console.WriteLine(s);
+        }
+
+        static string timeConversion(string s)
+        {
+            string[] time = s.Split(":");
+            if(time[time.Length - 1].Contains("PM"))
+            {
+                if(time[0] != "12")
+                {
+                    time[0] = (Int32.Parse(time[0]) + 12).ToString();
+                }
+            }
+            else
+            {
+                if(time[0] == "12")
+                {
+                    time[0] = "00";
+                }
+            }
+
+            s = string.Join(":", time);
+
+            return s.Substring(0, 8);
+        }
+    }
+}
